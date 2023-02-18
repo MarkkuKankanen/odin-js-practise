4723c43ed9acaa5838efa51968e4d26bc9818771
MarkkuKankanen <markku.kankanen@gmail.com>
2023-02-18 11:24:29 +0200

    Added function to ramdomize value for the computer

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
 function getComputerChoice() {
-    
+    const gameValues = new Array("Rock", "Paper", "Scissors");
+
+    const randomizedGameValue = gameValues[Math.floor(Math.random()*gameValues.length)];
+
+    return randomizedGameValue;
 }
+
+console.log(getComputerChoice());
